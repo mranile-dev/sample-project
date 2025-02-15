61036ec705d6c131920a0d76517fb7ec90dbfd8b
mrani <mrani@MikeR-TP-50.gateway>
2025-02-15 21:09:54 +0800

    First commit

---
+++ src/main/java/com/miker/sonarqube/sample/SampleRestController.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.miker.sonarqube.sample;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ */
+@RestController
+public class SampleRestController {
+
+}
