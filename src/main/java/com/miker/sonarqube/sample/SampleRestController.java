942b20c217fcbb537027961b2858c3ee78920cae
mranile-dev <51887498+mranile-dev@users.noreply.github.com>
2025-02-15 22:48:02 +0800

    Update SampleRestController.java

--- src/main/java/com/miker/sonarqube/sample/SampleRestController.java
+++ src/main/java/com/miker/sonarqube/sample/SampleRestController.java
@@ -10,5 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class SampleRestController {
-
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
 }
