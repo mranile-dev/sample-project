7af3dddfb2b12846757568b8e9380552c422aa18
mrani <mrani@MikeR-TP-50.gateway>
2025-02-15 23:14:23 +0800

    sql injection

--- src/main/java/com/miker/sonarqube/sample/SampleRestController.java
+++ src/main/java/com/miker/sonarqube/sample/SampleRestController.java
@@ -11,5 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleRestController {
   public static void main(String[] args) {
     System.out.println("Hello");
+    var sql = "SELECT * FROM TABLE1 WHERE NAME=" + args[0];
   }
 }
